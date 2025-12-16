260a18afc1beeca1066ed593999c024de8b991f6
ninthevencry <vince.henry@me.com>
2025-12-16 14:12:09 +0000

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/gridselectionapp/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
